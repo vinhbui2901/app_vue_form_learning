25457a74f98d9d866411f874e13ca217678cb280
Bùi Thanh Vĩnh <vinhbt@vietsoftpro.com>
2023-08-21 08:24:50 +0700

    form demo

--- src/components/app_form/AppForm.vue
+++ src/components/app_form/AppForm.vue
@@ -11,6 +11,37 @@
               <input type="password" class="form-control" id="Password">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="Hà Nội" v-model="listCheckBox">
+                <label class="form-check-label">
+                  Hà Nội
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value=" Đà Nẵng" v-model="listCheckBox">
+                <label class="form-check-label" >
+                  Đà Nẵng
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="HCM" v-model="listCheckBox">
+                <label class="form-check-label" >
+                  HCM
+                </label>
+              </div>
+              <!-- radio buttom -->
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" value="Nam" v-model="radioButton">
+                <label class="form-check-label">
+                  Nam
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" value="Nữ" v-model="radioButton">
+                <label class="form-check-label">
+                  Nữ
+                </label>
+              </div>
           </form>
     </section>
 </template>
@@ -19,11 +50,15 @@ export default {
     data(){
         return{
             emailAddress: "",
+            listCheckBox: [],
+            radioButton: "",
         };
     },
     methods:{
         handleSubmit(){
             console.log(this.emailAddress);
+            console.log(this.listCheckBox);
+            console.log(this.radioButton);
         }
     }
 }
